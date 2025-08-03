fb1797eab835552f7ca40b9dc7a87098df60342d
socks1024 <3619907017@qq.com>
2025-08-03 20:03:01 +0800

    sfx & music & levels

--- Assets/Tools/DataPersistence/PlayerPref/UIHandler/PlayerPrefSavableToggle.cs
+++ Assets/Tools/DataPersistence/PlayerPref/UIHandler/PlayerPrefSavableToggle.cs
@@ -6,7 +6,7 @@ namespace Tools.DataPersistence.PlayerPref {
         public override void ConnectEvent(Toggle eventHandler)
         {
             base.ConnectEvent(eventHandler);
-            eventHandler.onValueChanged.AddListener(HandlerWriteValue);
+            eventHandler.onValueChanged.AddListener((f) => HandlerWriteValue?.Invoke(f));
         }
 
         protected override bool ConvertValue(string value)
